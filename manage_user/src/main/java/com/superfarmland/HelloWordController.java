072a694d91e5f3688a91e857cd9a4af69db45271
zengzhuangzhuang <17775076122@163.com>
2021-03-25 22:05:11 +0800

    提交人：zengzhuangzhuang 提交原因：后端服务系统搭建 提交时间：2021-03-25

--- manage_user/src/main/java/com/superfarmland/HelloWordController.java
+++ manage_user/src/main/java/com/superfarmland/HelloWordController.java
@@ -1,11 +1,8 @@
 package com.superfarmland;
 
-import com.sun.xml.internal.ws.developer.MemberSubmissionAddressing;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
 @RestController
 @RequestMapping("/HelloWordController")
